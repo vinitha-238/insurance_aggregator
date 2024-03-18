13bb1592b5d8891f1194fb3e2e50b68854e9b8e2
vinitha-238 <132841994+vinitha-238@users.noreply.github.com>
2024-03-18 17:45:37 +0530

    Delete HealthInsurancePlanRepository.java

--- HealthInsurancePlanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.healthinsurance.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.healthinsurance.entity.HealthInsurancePlan;
-
-public interface HealthInsurancePlanRepository extends JpaRepository<HealthInsurancePlan, Long>  {
-
-}
